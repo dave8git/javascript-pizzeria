a17acc093d82945751dbf3639abb9a74357d42da
dave8git@gmail.com <dave8git@gmail.com>
2020-04-23 14:43:50 +0200

    Add all changes made up to adding the accordion

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,43 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);// generate HTML based on template
+      //console.log('GENERATEHTML:', generatedHTML);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML); // create element using utils.createElementFromHTML
+      const menuContainer = document.querySelector(select.containerOf.menu);// find menu container
+      menuContainer.appendChild(thisProduct.element);// add element to menu
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      //const testProduct = new Product();
+      //console.log('testProduct:', testProduct);
+      //const thisApp = this;
+      //thisApp.data = dataSource;
+      //console.log('thisApp.data:', thisApp.data);
+      const thisApp = this;
+
+      console.log('thisApp.data:', this.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +96,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
-
   app.init();
 }
