31fd1da7dacdf117c1622da92615b324f8e81ad9
dave8git <dave8git@gmail.com>
2020-04-23 13:48:14 -0700

    Add accordion added

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -67,6 +68,20 @@
       thisProduct.element = utils.createDOMFromHTML(generatedHTML); // create element using utils.createElementFromHTML
       const menuContainer = document.querySelector(select.containerOf.menu);// find menu container
       menuContainer.appendChild(thisProduct.element);// add element to menu
+    }
+    initAccordion() {
+      const thisProduct = this;
+      const buttonClicked = thisProduct.element.querySelector(select.menuProduct.clickable); // find the clicable trigger (the element that shoudl react to clicking )
+      buttonClicked.addEventListener('click', function(event) { // START: click event listener to trigger 
+        event.preventDefault(); // prevent default action for event
+        thisProduct.element.classList.toggle("active"); // toggle active class on element of thisProduct
+        const allActiveProducts = document.querySelectorAll('active'); // find all active products 
+        for (let activeProduct of allActiveProducts) {// START LOOP: for each active product 
+          if(activeProduct != thisProduct.element) {//START: if the active product isn't the element of thisProduct 
+            activeProduct.classList.remove('active');// remove class active for the active product
+         }// END: if the active product isn't the elemnt of thisProduct
+        }// END LOOP: for each active product 
+      }); // END: click event listener to trigger
     }
   }
 
