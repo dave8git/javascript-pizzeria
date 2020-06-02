5685e5a928472aa939649c303db7b8b58d66310b
dave8git@gmail.com <dave8git@gmail.com>
2020-06-02 19:45:18 +0200

    Add inheritance to AmountWidget from BaseWidget class

--- src/js/components/amountWidget.js
+++ src/js/components/amountWidget.js
@@ -3,10 +3,10 @@ import {BaseWidget} from './BaseWidget.js';
 
 export class AmountWidget extends BaseWidget {
   constructor(wrapper) {
-    super(element, settings.amountWidget.defaultValue);
+    super(wrapper, settings.amountWidget.defaultValue);
     const thisWidget = this;
-    
-    thisWidget.getElements(); 
+
+    thisWidget.getElements();
     thisWidget.initActions();
 
   }
@@ -17,39 +17,27 @@ export class AmountWidget extends BaseWidget {
     thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
-  setValue(value){
-    const thisWidget = this;
-    let newValue = parseInt(value);
-    /* TODO: Add validation */
-    if(newValue >= 10) {
-      newValue = 10;
-    } else if(newValue <= 1) {
-      newValue = 1;
-    }
-    if((thisWidget.value != newValue) && (newValue >= settings.amountWidget.defaultMin) && (newValue <= settings.amountWidget.defaultMax)) {
-      thisWidget.value = newValue;
-      thisWidget.announce();
-    }
-
-    thisWidget.input.value = thisWidget.value;
-  }
   isValid(newValue){
     return !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax;
   }
   initActions() {
     const thisWidget = this;
-    thisWidget.input.addEventListener('change', function() {
-      thisWidget.setValue(thisWidget.input.value);
+    thisWidget.dom.input.addEventListener('change', function() {
+      thisWidget.value = thisWidget.dom.input.value;
     });
-    thisWidget.linkDecrease.addEventListener('click', function(event){
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event){
       event.preventDefault();
       console.log('VALUE:', thisWidget.value);
-      thisWidget.setValue((thisWidget.value) - 1);
+      thisWidget.value--;
     });
-    thisWidget.linkIncrease.addEventListener('click', function(event){
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
       event.preventDefault();
       console.log(thisWidget.value);
-      thisWidget.setValue((thisWidget.value) + 1);
+      thisWidget.value++;
     });
+  }
+  renderValue() {
+    const thisWidget = this;
+    thisWidget.dom.input.value = thisWidget.value;
   }
 }
